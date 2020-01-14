8da67fecc38bedbe9255ba46fe341a65c0416955
maav <roammsavvie@gmail.com>
2020-01-14 16:10:24 +0100

    Successful Dictionary
    but not intiuituve

--- Python_assignment_14jan2020.py
+++ Python_assignment_14jan2020.py
@@ -8,9 +8,23 @@ with open('precipitation.json', encoding = 'utf8') as file:
 # we want to sort through the list to idenitfy only station US1WAKG0038
 # my challenge is to find a way to read the station number from within
 
-seattle_station_data = {}
 seattle_station_code = 'GHCND:US1WAKG0038'
 
-for generic_raindata in rainfall_info:
-    if (generic_raindata['station']) == seattle_station_code:
-        print(generic_raindata)
+month_data = {}
+
+for generic_raindata in rainfall_info:  # For each measurement 
+    if (generic_raindata['station']) == seattle_station_code:  # If from Seattle
+        month = generic_raindata['date'][5:7]
+        # print(month)
+        rainfall = generic_raindata['value']
+        # print(rainfall)
+        if month in month_data.keys():
+            month_data[month] += rainfall
+        else:
+            month_data[month] = rainfall
+
+        
+print(month_data)
+
+
+
